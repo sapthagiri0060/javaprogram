4b135df54dcef7b416afeddbc297f9f0fd848786
Sailendra3456 <sailendrachintu86@gmail.com>
2023-03-06 09:52:58 +0530

    Create Test10.java
    completed

---
+++ Test10.java
@@ -0,0 +1,35 @@
+class mynewth extends Thread {
+	String name;
+	mynewth(String n) {
+		name=n;
+	}
+	public void run() {
+		try {
+		System.out.println(name+ "running");
+		System.out.println("Welcome");
+		Thread.sleep(1000);
+		System.out.println("presidency");
+		}
+		catch(Exception e) {}
+	
+	}
+}
+public class Test10 {
+
+	public static void main(String[] args) throws Exception{
+		System.out.println("hi");
+
+		mynewth t1=new mynewth("first");
+		t1.start();
+		mynewth t2=new mynewth("second");
+		t2.start();
+		mynewth t3=new mynewth("third");
+		t3.start();
+		mynewth t4=new mynewth("fourth");
+		t4.start();
+		Thread.sleep(10000);
+		System.out.println("bye");
+
+	}
+
+}
